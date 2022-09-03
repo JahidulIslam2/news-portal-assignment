6dd6b1c895a8e2bbd8e6dda425196e3ee8327154
Jahidul Islam <jahid2111@gmail.com>
2022-09-04 03:52:49 +0600

    60%complete

--- newsSite.js
+++ newsSite.js
@@ -7,16 +7,16 @@ const catagories=()=>{
     .catch((error) => {console.log(error)})
 }
 
-// 
+// catagory name
 const getCatagories=(newsData)=>{
+  console.log(newsData)
     const getParentDiv=document.getElementById('btn-container');
     newsData.forEach(news => {
         const createElement=document.createElement('div');
         createElement.classList.add('div')
         createElement.innerHTML=`
-                    <a class="navbar-brand" href="#">${news.category_name}</a>
-                    
-        
+                    <a onclick="showCatagoryNews(${news.category_id})" class="navbar-brand active" href="#">${news.category_name}</a>
+
                     `;
                     getParentDiv.appendChild(createElement)
     });
@@ -24,9 +24,10 @@ const getCatagories=(newsData)=>{
 }
 
 
-const showCatagoryNews =()=>{
 
-    fetch(`https://openapi.programming-hero.com/api/news/category/01`)
+//news api
+const showCatagoryNews =(NewsShow)=>{
+    fetch(`https://openapi.programming-hero.com/api/news/category/0${NewsShow}`)
     .then(res => res.json())
     .then(data => NewsInCard(data.data))
     .catch((error) => {console.log(error)})
@@ -42,16 +43,22 @@ console.log(cardNews)
     cardNews.forEach(cardItem => {
         const createCardDiv=document.createElement('div');
         createCardDiv.innerHTML=`
-        <div class="card mb-3 mt-5" style="max-width: 70%;">
+        <div class="card mb-3 mt-5"> 
         <div class="row g-0">
-          <div class=" col-lg-4 col-md-8 col-sm-4 col-xs-4">
-            <img src="${cardItem.image_url}" class="img-fluid rounded-start h-100% w-auto"  alt="...">
+          <div class=" col-lg-4 col-md-4 col-sm-3 col-xs-3 h-100%">
+            <img src="${cardItem.thumbnail_url}" class="img-fluid h-100% w-auto  alt="...">
           </div>
           <div class="col-md-8 col-lg-8 col-sm-8 col-xs-12">
             <div class="card-body">
               <h5 class="card-title">${cardItem.title}</h5>
-              <p class="card-text" style="width: 100%;  overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">${cardItem.details}</p>
-              <p></p>
+                <p class="card-text" style="width: 100%;  overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">${cardItem.details}</p>
+           <div class=" col-lg-1 col-md-1 col-sm-1 col-xs-2 align-content-start d-inline g-5 ">
+              <img src="" class="img-thumbnail align-content-end border-radius:100%" alt="...">
+                <p>${cardItem.author.name == "system" ? "no data available" :cardItem.author.name}</p>
+                <p>${cardItem.author.published_date}</p>
+                <p>Total Views: ${cardItem.total_view}</p>
+              </div>
+                
             </div>
           </div>
         </div> 
